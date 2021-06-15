9263afa5fe9b897558e5bb70e4351ec464ca73b6
Giovanni Izzi <giovanni9972@libero.it>
2021-06-15 09:03:37 +0200

    initial commit

---
+++ apps/base-apparel-coming-soon-page/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'base-apparel-coming-soon-page-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'base-apparel-coming-soon-page';
+}
