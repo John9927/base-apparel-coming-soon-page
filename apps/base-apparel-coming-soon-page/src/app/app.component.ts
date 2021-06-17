e691d1242d0419d2b4ba22eaf8938fb1e7f646e2
Giovanni Izzi <giovanni9972@libero.it>
2021-06-17 09:57:07 +0200

    add font awesome + add validation input

--- apps/base-apparel-coming-soon-page/src/app/app.component.ts
+++ apps/base-apparel-coming-soon-page/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'base-apparel-coming-soon-page-root',
@@ -7,8 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base-apparel-coming-soon-page';
-
-  onClickSubmit(email: any) {
-    console.log(email)
+  form: any;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
   }
+
+  createForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+    });
+  }
+
+  onClickSubmit() {
+    console.log(this.form.controls.email.value);
+  }
+
 }
